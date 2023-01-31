421e87e7bce96513e6b72bc02a18c84f08427305
lukasz protosawicki <lukas.es.p@o2.pl>
2023-01-31 12:33:58 +0100

    add initial create, read update components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-	return <div className="main">React Crud Operations</div>;
+	return (
+		<div className="main">
+			<h2 className="main-header">React Crud Operations</h2>
+		</div>
+	);
 }
 
 export default App;
